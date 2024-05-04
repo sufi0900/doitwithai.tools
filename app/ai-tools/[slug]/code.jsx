7c7f13dc198cbba3c7ed5d64bfb841c76332809d
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-04 16:32:17 +0500

    first commit

--- app/ai-tools/[slug]/code.jsx
+++ app/ai-tools/[slug]/code.jsx
@@ -489,8 +489,8 @@ export default function BlogSidebarPage({ data, params, currentCategory  }) {
                               src={urlForImage(data.mainImage).url()}
                               alt=""
                               layout="responsive"
-                              width={originalWidth}
-                              height={originalHeight}
+                              width={500} 
+                              height={500}
                               placeholder="blur"
                               blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
                         
