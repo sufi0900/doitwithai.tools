5e3043b294eb1869ccf073dfb7fefb5cff936d72
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-08 20:44:40 +0500

    first commit

---
+++ app/lib/sanity.js
@@ -0,0 +1,17 @@
+// lib/sanity.js or a similar file where you setup Sanity client
+import { createClient } from "next-sanity";
+
+export const client = createClient({
+  projectId: 'gglvlxzt', // find this at manage.sanity.io or your sanity.json
+  dataset: 'production', // or the name of your dataset
+  useCdn: false, // `false` if you want to ensure fresh data
+});
+
+async function fetchURLs() {
+  const query = `*[_type in ["makemoney", "aitool", "news", "coding", "freeairesources", "seo"]] {
+    "slug": slug.current,
+    _type
+  }`;
+  const posts = await client.fetch(query);
+  return posts;
+}
