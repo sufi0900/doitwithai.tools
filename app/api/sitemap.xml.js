0cbf04a40173f54fd6f85f094cfa86107afc35ae
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-08 20:37:25 +0500

    first commit

--- app/api/sitemap.xml.js
+++ app/api/sitemap.xml.js
@@ -1,33 +1,43 @@
-import { fetchURLs } from "../../app/lib/sanity"; // Adjust the import according to your project structure
+import React from 'react'
 
-const createSitemap = (posts) => {
-  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
-
-  posts.forEach(post => {
-    sitemap += `
-      <url>
-        <loc>${`https://sufi-blog-website.vercel.app/${post._type}/${post.slug}`}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.9</priority>
-      </url>
-    `;
-  });
-
-  sitemap += `</urlset>`;
-  return sitemap;
-};
-
-export default async function sitemap(req, res) {
-  try {
-    const posts = await fetchURLs(); // Fetch all relevant URLs
-    const sitemap = createSitemap(posts);
-
-    res.setHeader("Content-Type", "text/xml");
-    res.write(sitemap);
-    res.end();
-  } catch (e) {
-    console.log(e);
-    res.send(JSON.stringify(e));
-  }
+const sitemap = () => {
+  return (
+    <div>sitemap</div>
+  )
 }
+
+export default sitemap
+
+// import { fetchURLs } from "../../app/lib/sanity"; // Adjust the import according to your project structure
+
+// const createSitemap = (posts) => {
+//   let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+//   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+//   posts.forEach(post => {
+//     sitemap += `
+//       <url>
+//         <loc>${`https://sufi-blog-website.vercel.app/${post._type}/${post.slug}`}</loc>
+//         <changefreq>weekly</changefreq>
+//         <priority>0.9</priority>
+//       </url>
+//     `;
+//   });
+
+//   sitemap += `</urlset>`;
+//   return sitemap;
+// };
+
+// export default async function sitemap(req, res) {
+//   try {
+//     const posts = await fetchURLs(); // Fetch all relevant URLs
+//     const sitemap = createSitemap(posts);
+
+//     res.setHeader("Content-Type", "text/xml");
+//     res.write(sitemap);
+//     res.end();
+//   } catch (e) {
+//     console.log(e);
+//     res.send(JSON.stringify(e));
+//   }
+// }
