3be12bb746ee7f3e60f8c9f9a09ebe069babc848
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-08 21:02:30 +0500

    first commit

--- app/api/sitemap.xml/page.js
+++ app/api/sitemap.xml/page.js
@@ -1,3 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
 // import { fetchURLs } from "../../../app/lib/sanity"; // Adjust the import according to your project structure
 
 // const createSitemap = (posts) => {
