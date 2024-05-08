ed017823ae1dcd6fcae9942ac645dc059df4a2d1
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-08 20:37:59 +0500

    first commit

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,31 +1,9 @@
-// pages/api/sitemap.xml.js
-import { client } from '@/sanity/lib/client';
+import React from 'react'
 
-const createSitemapXML = (posts) => {
-  const baseUrl = 'https://sufi-blog-website.vercel.app';
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${posts.map(post => `
-    <url>
-      <loc>${baseUrl}/${post._type}/${post.slug.current}</loc>
-      <lastmod>${new Date(post._updatedAt).toISOString()}</lastmod>
-      <changefreq>weekly</changefreq>
-      <priority>0.9</priority>
-    </url>`).join('')}
-</urlset>`;
-  return sitemap;
-};
-
-export default async function sitemapHandler(req, res) {
-    try {
-        const query = '*[_type in ["makemoney", "aitool", "news", "coding", "freeairesources", "seo"]]{_type, "slug": slug.current, _updatedAt}';
-        const posts = await client.fetch(query);
-        const sitemapXML = createSitemapXML(posts);
-
-        res.setHeader('Content-Type', 'application/xml');
-        res.status(200).send(sitemapXML);
-    } catch (error) {
-        console.error("Failed to generate sitemap:", error);
-        res.status(500).send("Error generating sitemap.");
-    }
+const sitemap = () => {
+  return (
+    <div>sitemap</div>
+  )
 }
+
+export default sitemap
