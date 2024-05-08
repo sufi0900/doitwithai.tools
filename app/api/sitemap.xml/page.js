58a46195e8c5d75aed218a1771b32f57ab2b2135
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-08 21:20:59 +0500

    first commit

--- app/api/sitemap.xml/page.js
+++ app/api/sitemap.xml/page.js
@@ -1,46 +1,33 @@
-import React from 'react'
+import { fetchURLs } from "../../lib/sanity";
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+const SITE_URL = 'https://sufi-blog-website.vercel.app';
+
+const createSitemap = (posts) => {
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${posts.map(post => `
+        <url>
+          <loc>${`${SITE_URL}/${post._type}/${post.slug}`}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>weekly</changefreq>
+          <priority>0.8</priority>
+        </url>
+      `).join('')}
+    </urlset>
+  `;
+  return sitemap;
+};
+
+export default async function sitemap(req, res) {
+  try {
+  const posts = await fetchURLs(); 
+  const sitemap = createSitemap(posts);
+  res.setHeader("Content-Type", "text/xml");
+  res.status(200).send(sitemap);
+
+} catch (e) {
+  console.error(e);
+  res.status(500).json({ error: 'Failed to generate sitemap.' });
 }
-
-export default page
-// import { fetchURLs } from "../../../app/lib/sanity"; // Adjust the import according to your project structure
-
-// const createSitemap = (posts) => {
-//   let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-//   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
-
-//   posts.forEach(post => {
-//     sitemap += `
-//       <url>
-//         <loc>${`https://sufi-blog-website.vercel.app/${post._type}/${post.slug}`}</loc>
-//         <changefreq>weekly</changefreq>
-//         <priority>0.9</priority>
-//       </url>
-//     `;
-//   });
-
-//   sitemap += `</urlset>`;
-//   return sitemap;
-// };
-
-// export default async function sitemap(req, res) {
-//   try {
-//     const posts = await fetchURLs(); // Fetch all relevant URLs
-//     const sitemap = createSitemap(posts);
-
-//     res.setHeader("Content-Type", "text/xml");
-//     res.write(sitemap);
-//     res.end();
-//   } catch (error) {
-//     console.error('Error generating sitemap:', error);
-//     if (res && res.status) {
-//       res.status(500).send(JSON.stringify(error));
-//     } else {
-//       console.error('Unable to send error response: Response object not properly defined.');
-//     }
-//   }
-// }
+}
