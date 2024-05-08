f1f012d46adc433eab100fb24672e725e05eabb1
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-08 18:08:15 +0500

    first commit

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,30 +1,9 @@
-export const siteUrl = 'https://sufi-blog-website.vercel.app';
-export const generateRobotsTxt = true;
-export const sitemapSize = 7000;
-export const outDir = './public';
-export async function transform(config, paths) {
-    // Fetch all posts or items you need from Sanity.io
-    const fetchedSchemas = await getAllSlugsFromSanity(); // Define this function to fetch your data
-    const allPages = fetchedSchemas.map(slug => {
-        return { loc: `${config.siteUrl}/${slug}`, changefreq: 'daily', priority: 0.7 };
-    });
+import React from 'react'
 
-    return [...paths, ...allPages];
+const sitemap = () => {
+  return (
+    <div>next-sitemap</div>
+  )
 }
-  
-  // An example function to fetch all unique pages' or posts' slugs across all schemas
-  async function getAllSlugsFromSanity() {
-    const client = require('./sanity/lib/client'); // Update to import your configured Sanity client
-  
-    const types = ["makemoney", "aitool", "news", "coding", "freeairesources", "seo"];
-  
-    let slugs = [];
-    for (let type of types) {
-      const query = `*[_type == "${type}"]{ "slug": slug.current }`;
-      const data = await client.fetch(query);
-      slugs = slugs.concat(data.map(item => `${type}/${item.slug}`));
-    }
-  
-    return slugs;
-  }
-  
+
+export default sitemap
