eae0c3abab07ebc6f0540663ce028b6b2c7997a7
sufi0900 <sufianmustafa0900@gmail.com>
2024-05-10 23:21:00 +0500

    first commit

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,3 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Breadcrumb from "@/components/Common/Breadcrumb";
 
 const AboutPage = () => {
   return (
