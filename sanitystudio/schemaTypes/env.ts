9025702d995af1a85870f0db10e1fbbaab4d2f21
sufi0900 <sufianmustafa0900@gmail.com>
2025-01-17 00:21:06 +0500

    first commit

--- sanitystudio/schemaTypes/env.ts
+++
@@ -1,9 +0,0 @@
-
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-30';
-
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'qyshio4a';
-
-export const useCdn = false;
