f9ba445b6981ceac039337bc72019ecbb39e2043
sufi0900 <sufianmustafa0900@gmail.com>
2025-01-17 23:14:32 +0500

    first commit

--- app/blog-sidebar/page.tsx
+++ app/blog-sidebar/page.tsx
@@ -1,25 +1,9 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { createContext } from 'react';
+import React from 'react'
 
-import RelatedPost from "@/components/Blog/RelatedPost";
-import SharePost from "@/components/Blog/SharePost";
-import TagButton from "@/components/Blog/TagButton";
-import NewsLatterBox from "@/components/Contact/NewsLatterBox";
-import Image from "next/image";
-
-
-export const metadata: Metadata = {
-  title: "Blog Details Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Blog Details Page for Startup Nextjs Template",
-  // other metadata
-};
-
-const BlogSidebarPage = () => {
+const page = () => {
   return (
-    <>
-     
-    </>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default BlogSidebarPage;
+export default page
