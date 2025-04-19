3b2d40820b4a4dc1cd8109342dfab94ffbe2405e
sufi0900 <sufianmustafa0900@gmail.com>
2025-04-19 16:13:00 +0500

    first commit

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,12 +1,12 @@
 import { type SchemaTypeDefinition } from "sanity";
-import { aitool } from "./ai-tools";
+import { aitool } from "./ai-tool";
 import { coding } from "./code";
 import { makemoney } from "./make-money";
-import { seo } from "./ai-seo";
+import { seo } from "./seo";
 import { news } from "./news";
 import { blog } from "./blogs";
 import { brands } from "./brands";
-import { seoSubcategory } from "./ai-seoSubcategory";
+import { seoSubcategory } from "./seoSubcategory";
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
