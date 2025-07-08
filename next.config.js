dd7ed3c488fd66cd91bc920a98f4abfd2ffb5812
sufian mustafa <sufianmustafa0900@gmail.com>
2025-07-09 00:00:29 +0500

    service worker checker

--- next.config.js
+++ next.config.js
@@ -1,30 +1,18 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 /** @type {import('next').NextConfig} */
+
+
+
+
 const nextConfig = {
-  reactStrictMode: false,
-  
-  // Add static generation config
-  experimental: {
-    isrMemoryCacheSize: 0, // Disable ISR memory cache
-  },
-  
-  // Add build-time environment handling
-  env: {
-    SKIP_BUILD_STATIC_GENERATION: process.env.SKIP_BUILD_STATIC_GENERATION || 'false',
-  },
-  
+    reactStrictMode: false, // This should be false to reduce duplicate effects in development
+
   images: {
     domains: ['your-sanity-domain.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
-    remotePatterns: [
+ 
+          remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
@@ -34,4 +22,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
