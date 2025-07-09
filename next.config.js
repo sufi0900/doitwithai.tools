ffda2baa46290e7e43fedff1633cccd16cdcf829
sufian mustafa <sufianmustafa0900@gmail.com>
2025-07-09 15:40:29 +0500

    service worker checker

--- next.config.js
+++ next.config.js
@@ -1,62 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: false, // We'll register manually
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  publicExcludes: ['!robots.txt', '!sitemap.xml'],
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/.*\.sanity\.io\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'sanity-api',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 24 * 60 * 60 * 7, // 7 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/cdn\.sanity\.io\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'sanity-images',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 24 * 60 * 60 * 30, // 30 days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:js|css|woff2?|png|jpg|jpeg|webp|svg|ico)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-resources',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 24 * 60 * 60 * 30, // 30 days
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/.*\.vercel\.app\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'vercel-pages',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 24 * 60 * 60, // 1 day
-        },
-      },
-    },
-  ],
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -73,12 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  // Important for Vercel Service Worker
-  experimental: {
-    serverActions: {
-      allowedOrigins: ['localhost:3000', '*.vercel.app']
-    }
-  }
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
