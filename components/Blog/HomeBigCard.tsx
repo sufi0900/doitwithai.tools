39cc24fa245b0adf9582195131c848b463c96c28
sufian mustafa <sufianmustafa0900@gmail.com>
2025-08-19 02:13:15 +0500

    Everything Done ✅✅

--- components/Blog/HomeBigCard.tsx
+++ components/Blog/HomeBigCard.tsx
@@ -78,9 +78,10 @@ export default function HomeBigCard({
            enableModal={false}
 
           >
+                </ImageOptimizer>
           {/* Gradient Overlay on Hover */}
           <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-        </ImageOptimizer>
+    
         </CardMedia>
                        
         {/* Enhanced Tag */}
